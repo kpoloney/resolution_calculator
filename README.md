39407321f6a9fd95c29896d7b2b09834988be33f
Kelsey Poloney <poloke01@gmail.com>
2022-02-09 17:08:29 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Calculate scanning resolution
+
+This is a simple shiny app to calculate the dpi to scan images based on the original's measurement and the desired pixel length.
+
+## Usage
+
+The app can be accessed online at https://sfuarchives.shinyapps.io/resolution_calculator/
+
+It can be run locally using R and the shiny library using the command `shiny::runApp("app.R")`.
---
+++ app.R
@@ -0,0 +1,57 @@
+library(shiny)
+library(shinyWidgets)
+ui <- fluidPage(
+  theme = bslib::bs_theme(bootswatch = "litera"),
+  h2("Resolution Calculator"),
+  numericInput(
+    "orig_size",
+    label = "Length of longest size of original",
+    value = NA,
+    min = 0
+  ),
+  prettyRadioButtons(
+    "unit",
+    label = 'Unit',
+    choices = c("mm", "cm", "inches", "feet"),
+    inline = T
+  ),
+  numericInput(
+    "pixels",
+    label = "Target pixels",
+    value = NA,
+    min = 0
+  ),
+  actionButton("calc", label = "Calculate resolution", class = "btn-primary"),
+  actionButton("reset", label = "Reset"),
+  h3(textOutput("Resolution"))
+)
+
+server <- function(input, output, session) {
+  scan_res <- function() {
+    if (!is.na(input$orig_size) & !is.na(input$pixels)) {
+      size_inches <- switch(
+        input$unit,
+        mm = 0.039 * input$orig_size,
+        cm = 0.394 * input$orig_size,
+        inches = input$orig_size,
+        feet = 12 * input$orig_size
+      )
+      resolution <- input$pixels / size_inches
+      return(paste0(round(resolution), " dpi"))
+    } else
+      return(" ")
+  }
+  observeEvent(input$calc, {
+    output$Resolution <- renderText({
+      scan_res()
+    })
+  })
+  observeEvent(input$reset, {
+    updateNumericInput(session, "orig_size", value = NA)
+    updateNumericInput(session, "pixels", value = NA)
+  })
+  
+  
+}
+
+shinyApp(ui, server)
